112f64d95d28af1deb07641b93fe82929ac64c61
JoseJairo16 <rojassanchezjose2006@gmail.com>
2026-05-31 17:20:31 -0600

    Add files via upload

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // URL base del backend
-const BASE_URL = "http://localhost:4000/api";
+const BASE_URL = "https://api-backend-estacionamiento-production.up.railway.app/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
